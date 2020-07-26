ebbb171f1adf17a8d6ed54f0b3992aba19951273
Alexander450515 <62418411+Alexander450515@users.noreply.github.com>
2020-07-26 22:23:37 +0300

    1

--- project/static/app.js
+++ project/static/app.js
@@ -7,7 +7,7 @@ new Vue({
         const vm = this;
         axios.get('/api/orders/')
         .then(function(response){
-        console.log(response.data)
+        vm.orders = response.data
         })
     }
 }
